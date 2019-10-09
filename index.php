63f1c8d37001a5ca15c957036ed7db15975e47cd
Steph Mireault <sdbmireault@gmail.com>
2019-10-09 17:10:54 -0500

    Added new nav styles, background img, responsive design.

--- index.php
+++ index.php
@@ -16,24 +16,31 @@
 	<link href="styles.css" rel="stylesheet" type="text/css" />
 	<link rel="stylesheet" media="(max-width:600px)" href="styles_small.css" />
 	<link rel="stylesheet" media="(min-width:601px)" href="styles_large.css" />
+  <link href="https://fonts.googleapis.com/css?family=Titillium+Web&display=swap" rel="stylesheet">
+	<link href="https://fonts.googleapis.com/css?family=Turret+Road&display=swap" rel="stylesheet">
 	<link href="https://fonts.googleapis.com/css?family=Oswald&display=swap" rel="stylesheet"> 
+	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
 	</head>
 	<body>
-		<div id="container">
-		<header>
-			<nav>
-			<h2>Stephane Mireault</h2>
-				<div class="table">
-					<ul>
-						<li><a href="#">About</a></li>
-						<li><a href="#">Experience</a></li>
-						<li><a href="#">GitHub</a></li>
-					</ul>
-				</div>
-			</nav>
-		
-		</header>
+		<div class="background">
+		<div class="container">
+			<?php include'nav.php'; ?>
+
+			<div id="info">
+				<h2>TEST</h2>
+			</div>
 		</div>
-		
+		</div>
+
+		<script>
+		function myFunction() {
+			var x = document.getElementById("menu_items");
+			if (x.style.display === "block") {
+			x.style.display = "none";
+			} else {
+			x.style.display = "block";
+			}
+		} 
+		</script>
 	</body>
 </html>
---
+++ nav.php
@@ -0,0 +1,15 @@
+<header>
+			<nav>
+			<h2>Stephane Mireault</h2>
+				<div class="menu">
+					<ul id="menu_items">
+						<li><a href="#">About</a></li>
+						<li><a href="#">Experience</a></li>
+						<li><a href="https://www.github.com/syrissel">GitHub</a></li>
+					</ul>
+					<a href="javascript:void(0);" class="icon" onclick="myFunction()">
+    				<i class="fa fa-bars"></i>
+  				</a>
+				</div>
+			</nav>
+</header>
